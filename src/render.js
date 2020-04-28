d9d593fc1870c63aada0feb76e318a6e21ce279e
Danny Monroy <danny.p.monroy@gmail.com>
2020-04-28 10:47:07 +0100

    Grab folder paths

--- src/render.js
+++ src/render.js
@@ -1,1 +1,42 @@
+// Core Module Imports
+
+//Electron Imports
+const { dialog } = require('electron').remote;
+
+// Dom Handlers
+const btnSource = document.getElementById('source');
+const btnDestination = document.getElementById('destination');
+const btnStart = document.getElementById('start');
+
+//Global Variables
+let sourcePath;
+let destinationPath;
+
+
 console.log('Avocado Organiser');
+
+//event handlers
+
+btnSource.addEventListener('click', () => {
+  console.log("Source clicked");
+  dialog.showOpenDialog({
+    properties: ['openDirectory']
+  }).then((data) =>{
+    sourcePath = data.filePaths;
+  });
+})
+
+btnDestination.addEventListener('click', () => {
+  console.log("Destination clicked");
+  dialog.showOpenDialog({
+    properties: ['openDirectory']
+  }).then((data) =>{
+    destinationPath = data.filePaths;
+  });
+})
+
+btnStart.addEventListener('click', () => {
+  console.log("Start clicked");
+  console.log(`Source: ${sourcePath} | Destination: ${destinationPath}`);
+
+})
